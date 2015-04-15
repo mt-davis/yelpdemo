31058f236833be5e09f5856cccb27b6df676777c
M.T. Davis <marquese.davis@gmail.com>
2015-04-15 16:45:21 +0000

    Set User Premissions

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,7 @@
 class RestaurantsController < ApplicationController
   before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :check_user, except: [:index, :show]
 
   def search
   if params[:search].present?
@@ -81,3 +83,9 @@ end
       params.require(:restaurant).permit(:name, :address, :phone, :website, :image)
     end
 end
+
+def check_user
+    unless current_user.admin?
+        redirect_to root_url, alert: "Sorry, only admins can do that!"
+    end
+  end
